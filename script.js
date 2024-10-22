8d9f5af23448611be06107899ecc748c4fcc7997
maxmazurka <maks_lord_2020@mail.ru>
2024-10-22 05:58:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ document.getElementById("calcul").addEventListener("click", function () {
     const numb = document.getElementById("nmbr").value;
     const price = document.getElementById("prod").value;
     const finalprice = numb * price;
-    const incorrect = /^[1-9][0-9]*$/;
+    const correct = /^[1-9][0-9]*$/;
     
-    if (!incorrect.test(numb)) {
+    if (correct.test(numb)) {
         document.getElementById("result").textContent = finalprice + " рублей";
     } else {
         document.getElementById("result").textContent = "Некорректные данные в количестве товара"; 
